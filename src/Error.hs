f1fd83ad856a6d948d7939779c305c79b5dbea67
rbykov <rbykov04@gmail.com>
2025-02-17 12:56:40 +0300

    prepare Error to make it IO Free

--- src/Error.hs
+++ src/Error.hs
@@ -9,36 +9,49 @@ data Error = ErrorCode Int
            | ErrorToken Token String
            deriving Show
 
-errorAt :: String -> Int -> String -> IO (Int)
-errorAt current_input loc text = do
-  hPutStrLn stderr $ current_input
-  hPutStrLn stderr $ replicate loc ' ' ++ "^ " ++ text
-  return 1
 
-errorTok :: String -> Token -> String -> IO (Int)
-errorTok input tok text = errorAt input (tokenLoc tok) $ text ++ "\n"  ++ show tok
+printTextErr :: [String] -> IO (Int)
+printTextErr [] = return 1
+printTextErr (a : as) = do
+  hPutStrLn stderr a
+  printTextErr as
 
+bind3args out in3 = let
+    e = (.) out
+    g = (.) e
+    f = (.) g
+    in f in3
+
+errorAt' :: String -> Int -> String -> [String]
+errorAt' current_input loc text = [current_input] ++ [replicate loc ' ' ++ "^ " ++ text]
+
+errorTok' :: String -> Token -> String -> [String]
+errorTok' input tok text = errorAt' input (tokenLoc tok) $ text ++ "\n"  ++ show tok
+
+errorAt = bind3args printTextErr errorAt'
+errorTok = bind3args printTextErr errorTok'
+
+
+printError' :: String -> Error -> [String]
+printError' input (ErrorToken t text) =
+        let
+          lines = getLines input 0 0
+          iLines = zip[0..] lines
+          line = ignoreUnknownLine (find (finder (tokenLoc t)) iLines)
+          a = "stdin:" ++ show (fst line) ++ ": " ++ text ++ " " ++ (show (tokenKind t))
+          b = errorTok' input t text
+        in [a] ++ b
+          where
+            finder :: Int -> ((Int, (Int, Int)) -> Bool)
+            finder x (_, (_, e)) = x < e
+
+            -- FIXME: unreachiable - think about it
+            ignoreUnknownLine (Just x) = x
+            ignoreUnknownLine Nothing = (0,(0, 0))
+printError' input (ErrorLoc loc text) = errorAt' input loc text
+printError' input (ErrorText text) = [text]
 
 
 printError :: String -> Error -> IO (Int)
-printError input (ErrorToken t text) = do
-        let lines = getLines input 0 0
-        let iLines = zip[0..] lines
-        let line = ignoreUnknownLine (find (finder (tokenLoc t)) iLines)
-
-        hPutStrLn stderr $ "stdin:" ++ show (fst line) ++ ": " ++ text ++ " " ++ (show (tokenKind t))
-        errorTok input t text
-    where
-      finder :: Int -> ((Int, (Int, Int)) -> Bool)
-      finder x (_, (_, e)) = x < e
-
-      -- FIXME: unreachiable - think about it
-      ignoreUnknownLine (Just x) = x
-      ignoreUnknownLine Nothing = (0,(0, 0))
-printError input (ErrorLoc loc text) = do
-        errorAt input loc text
-        return 1
-printError input (ErrorText text) = do
-        let lines = getLines input 0 0
-        hPutStrLn stderr text
-        return 1
+printError input err = do
+  printTextErr (printError' input err)
---
+++ test/Parse2/simple.md
@@ -0,0 +1,29 @@
+# WIP
+Maybe I need special language to test rb-cc
+
+# program
+``` test
+program
+    x = in
+    y = parse2 x
+    out =
+end
+```
+
+# testCase1
+## in
+``` c
+int x;
+int y;
+```
+
+## 
+expect out
+-----------------
+vars int {
+    x : int
+}
+-----------------
+
+
+testCase2
