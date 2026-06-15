612f0617dc180985e7077496c8290d2b36f4442e
rbykov <rbykov04@gmail.com>
2026-06-15 16:50:16 +0300

    vm.c: init

--- test/hspec/VMSpec.hs
+++ test/hspec/VMSpec.hs
@@ -14,6 +14,13 @@ import Data.Either
 import Data.List
 import Data.Maybe
 import Text.Pretty.Simple
+
+import System.Environment
+import System.IO
+import System.Exit
+import System.Directory
+import System.Process
+import Control.Exception (finally)
 import Data.Text.Lazy (unpack)
 
 printErr err = unpack (pShow err)
@@ -31,3 +38,19 @@ spec = do
             let ir = toIR (head (objBody (storage ! 1)))
             ir `shouldBe` [PushInt 42, Ret]
             runVM ir `shouldBe` 42
+            case compile prog of
+              Left err -> expectationFailure (printErr (err))
+              Right asm -> do
+                writeFile "test_output.s" (unlines asm)
+                let action = do
+                      (ret, _ , cErr) <- readProcessWithExitCode "gcc" ["test_output.s", "-o", "test_prog"] ""
+                      case ret of
+                        ExitFailure c -> expectationFailure (printErr (cErr))
+                        ExitSuccess -> do
+                          (runExit, _ , _) <- readProcessWithExitCode "./test_prog" [] ""
+                          case runExit of
+                            ExitFailure code -> code `shouldBe` 42
+                            ExitSuccess -> expectationFailure "run must retut some code"
+                action `finally` do
+                  removeFile "test_output.s"
+                  removeFile "test_prog"
